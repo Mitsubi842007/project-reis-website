45c3b278e06ec2fbb3e64e951f16ad9eede4af86
deanvt1 <1219213@student.roc-nijmegen.nl>
2026-05-27 10:48:16 +0200

    html en css login aangepast
    ik heb de html en css van de login aangepast omdat het door elkaar stond

--- app/login.php
+++ app/login.php
@@ -4,56 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>log in</title>
-   <link rel="stylesheet" href="reisbureau.css">
+    <link rel="stylesheet" href="reisbureau.css">
 </head>
-  <body>
-    <script>
+<body>
 
-
-  
-</script>
-    <!--navigation menu -->
-    <nav class="navbar">
-      <div class="logo">
-        <div class="logo-icon"></div>
-        <h2>TungSahara</h2>
-      </div>
-      <div class="nav-menu">
-        <a href="index.php">Home</a>
-        <a href="informatie.php">Information</a>
-        <a href="boeking.php">Booking</a>
-        <a href="login.php" class="login-btn" class="active">Login</a>
-      </div>
-    </nav>
-<!--log in form -->
-<div class="card">
-    <div class="logo"></div>
-
-    <h1>Welkom Terug</h1>
-    <p>Log in om je boekingen te beheren</p>
-
-    <label for="email">E-mailadres</label>
-    <input type="email" id="email" placeholder="jouw@email.nl" />
-
-    <label for="wachtwoord">Wachtwoord</label>
-    <input type="password" id="wachtwoord" />
-
-    <div class="rij">
-      <label><input type="checkbox" checked /> Onthoud mij</label>
-      <a href="#">Wachtwoord vergeten?</a>
+  <!-- navigatie -->
+  <nav class="navbar">
+    <div class="brandName">
+      <img src="afbeeldingen/palm boom logo.png">
+      <h2>TungSahara</h2>
     </div>
+    <div class="nav-menu">
+      <a href="index.php">Home</a>
+      <a href="informatie.php">Information</a>
+      <a href="boeking.php">Booking</a>
+      <a href="login.php" class="login-btn active">Login</a>
+    </div>
+  </nav>
 
-    <button>Inloggen</button>
+  <!-- login -->
+  <div class="login-wrapper">
+    <div class="login-card">
+      <div class="login-logo">🌴</div>
 
-    <p class="onderaan">Nog geen account? <a href="#">Registreer hier</a></p>
+      <h1>Welkom Terug</h1>
+      <p>Log in om je boekingen te beheren</p>
+
+      <label for="email">E-mailadres</label>
+      <input type="email" id="email" placeholder="jouw@email.nl" />
+
+      <label for="wachtwoord">Wachtwoord</label>
+      <input type="password" id="wachtwoord" />
+
+      <div class="rij">
+        <label><input type="checkbox" checked /> Onthoud mij</label>
+        <a href="#">Wachtwoord vergeten?</a>
+      </div>
+
+      <button>Inloggen</button>
+
+      <p class="onderaan">Nog geen account? <a href="#">Registreer hier</a></p>
+    </div>
   </div>
-<!--footer -->
+
+  <!-- footer -->
   <footer>
     <p>© 2026 TungSahara. Ontdek de magie van de Sahara.</p>
     <a href="algemeneVoorwaarden.php">algemene voorwaarden</a>
   </footer>
-  </div>
 
 </body>
-
-</html>
+</html> 
