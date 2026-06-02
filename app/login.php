f4b1449cd072e9c48f5b296ef23bfd95aa09bf7d
Mitsubi <agp.842007@gmail.com>
2026-06-02 13:31:31 +0200

    login, css
    i updated some login from the other branch and css is fully updated

--- app/login.php
+++ app/login.php
@@ -4,15 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>log in</title>
-   <link rel="stylesheet" href="reisbureau.css">
+    <link rel="stylesheet" href="reisbureau.css">
 </head>
-  <body>
-    <script>
-
-
-  
-</script>
-   <!--navigation menu-->
+<body>
+ 
+  <!-- navigatie -->
   <nav class="navbar">
     <div class="brandName">
       <img src="afbeeldingen/palm boom logo.png">
@@ -22,19 +18,37 @@
       <a href="index.php">Home</a>
       <a href="informatie.php">Information</a>
       <a href="boeking.php">Booking</a>
-      <a href="login.php" class="login-btn ">Login</a>
+      <a href="login.php" class="login-btn active">Login</a>
     </div>
   </nav>
-  <!--achtergrond -->
  
-<!--log in form -->
-<!--footer -->
+  <!-- login -->
+  <div class="login-wrapper">
+    <div class="login-card">
+     
+ 
+      <h1>Welkom Terug</h1>
+      <p>Log in om je boekingen te beheren</p>
+ 
+      <label for="email">E-mailadres</label>
+      <input type="email" id="email" placeholder="jouw@email.nl" />
+ 
+      <label for="wachtwoord">Wachtwoord</label>
+      <input type="password" id="wachtwoord" />
+ 
+     
+     
+      <button>Inloggen</button>
+<p class="onderaan">Login met admin <a href="#">Klik hier</a></p>
+     
+    </div>
+  </div>
+ 
+  <!-- footer -->
   <footer>
     <p>© 2026 TungSahara. Ontdek de magie van de Sahara.</p>
     <a href="algemeneVoorwaarden.php">algemene voorwaarden</a>
   </footer>
-  </div>
-
+ 
 </body>
-
 </html>
