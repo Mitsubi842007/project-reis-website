a0e9d50267efcb7e8b0a2780dfd237348244667b
Mitsubi <agp.842007@gmail.com>
2026-06-02 13:45:58 +0200

    pdo php
    added the basics for my pdo

---
+++ app/pdo.php
@@ -0,0 +1,20 @@
+<?php
+$host = "db";
+$db = "mydatabase";
+$email = "email";
+$password = "password";
+$charset = "utf8mb4";
+
+$opties = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+    $pdo = new PDO($dsn, $email, $password,);
+} catch (PDOException $e) {
+    die("Unable to connect to the database.");
+}
