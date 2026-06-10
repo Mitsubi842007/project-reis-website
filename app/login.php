66ed487f98c51c995240fab4b7c44d2ff72bcce0
deanvt1 <1219213@student.roc-nijmegen.nl>
2026-06-10 08:48:44 +0200

    restore

--- app/login.php
+++ app/login.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+<<<<<<< Updated upstream
 
 <head>
+=======
+ 
+<head>
+<<<<<<< HEAD
+>>>>>>> Stashed changes
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>log in</title>
   <link rel="stylesheet" href="reisbureau.css">
+<<<<<<< Updated upstream
 </head>
 
 <body>
 
+=======
+</head>
+ 
+=======
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>log in</title>
+    <link rel="stylesheet" href="reisbureau.css">
+</head>
+>>>>>>> bbc0f8e989dae52eb90aaf627dc788a2cfd75e99
+<body>
+ 
+>>>>>>> Stashed changes
   <!-- navigatie -->
   <nav class="navbar">
     <div class="brandName">
@@ -23,6 +43,7 @@
       <a href="login.php" class="login-btn active">Login</a>
     </div>
   </nav>
+<<<<<<< Updated upstream
 
   <!-- login -->
   <div class="login-wrapper">
@@ -46,13 +67,61 @@
     </div>
   </div>
 
+=======
+ 
+  <!-- login -->
+  <div class="login-wrapper">
+    <div class="login-card">
+<<<<<<< HEAD
+ 
+=======
+     
+>>>>>>> bbc0f8e989dae52eb90aaf627dc788a2cfd75e99
+ 
+      <h1>Welkom Terug</h1>
+      <p>Log in om je boekingen te beheren</p>
+ 
+      <label for="email">E-mailadres</label>
+      <input type="email" id="email" placeholder="jouw@email.nl" />
+ 
+      <label for="wachtwoord">Wachtwoord</label>
+      <input type="password" id="wachtwoord" />
+ 
+<<<<<<< HEAD
+ 
+ 
+      <button>Inloggen</button>
+      <p class="onderaan">Login met admin <a href="#">Klik hier</a></p>
+ 
+=======
+     
+     
+      <button>Inloggen</button>
+<p class="onderaan">Login met admin <a href="#">Klik hier</a></p>
+     
+>>>>>>> bbc0f8e989dae52eb90aaf627dc788a2cfd75e99
+    </div>
+  </div>
+ 
+>>>>>>> Stashed changes
   <!-- footer -->
   <footer>
     <p>© 2026 TungSahara. Ontdek de magie van de Sahara.</p>
     <a href="algemeneVoorwaarden.php">algemene voorwaarden</a>
   </footer>
+<<<<<<< Updated upstream
   <script src="script.js"></script>
 
 </body>
 
 </html>
+=======
+ 
+</body>
+<<<<<<< HEAD
+ 
+</html>
+=======
+</html>
+>>>>>>> bbc0f8e989dae52eb90aaf627dc788a2cfd75e99
+>>>>>>> Stashed changes
