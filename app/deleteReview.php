21b9e5190ca3811e15b8d46f8e521912a3caeca5
deanvt1 <1219213@student.roc-nijmegen.nl>
2026-06-24 10:51:52 +0200

    review
    weg

--- app/deleteReview.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'pdo.php';
-$id = $_GET['id'];
-$sql = "DELETE FROM reviews WHERE id = ?";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$id]);
-header("Location: admin.php");
-exit();
